f46a8dbb70679ed3b89a12eeb382d8e832951abf
fridah kim <fredakim18@gmail.com>
2018-12-05 14:33:58 +0300

    add js file updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,53 @@
+// Business (or back-end) logic:
 var add = function(number1, number2) {
      return number1 + number2;
    };
-   
-   var number1 = parseInt(prompt("Enter a number:"));
-   var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+     return number1 - number2;
+   };
+var multiply = function(number1, number2) {
+     return number1 * number2;
+   };
+var divide = function(number1, number2) {
+     return number1 / number2;
+   };
+// User interface (or front-end) logic:
+   $(document).ready(function(){
+   $("form#add").submit(function(event){
+   event.preventDefault();
+   var number1 = parseInt($("#add1").val());
+   var number2 = parseInt($("#add2").val());
    var result = add(number1, number2);
-   alert(result);
+   $("#output").text(result);
+
+   $("form#add").submit(function(event){
+     event.preventDefault();
+   var number1 = parseInt($("#sub1").val());
+   var number2 = parseInt($("#sub2").val());
+   var result = subtract(number1, number2);
+   $("#output").text(result);
+
+   $("form#add").submit(function(event){
+     event.preventDefault();
+   var number1 = parseInt($("#div1").val());
+   var number2 = parseInt($("#div2").val());
+   var result = divide(number1, number2);
+   $("#output").text(result);
+
+   $("form#add").submit(function(event){
+     event.preventDefault();
+   var number1 = parseInt($("#mult1").val());
+   var number2 = parseInt($("#mult2").val());
+   var result = multiply(number1, number2);
+   $("#output").text(result);
+
+
+
+
+
+
+
+
+
+   });
+   });
